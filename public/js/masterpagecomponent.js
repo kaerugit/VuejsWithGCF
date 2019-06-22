1b99f9f688456ff5196213c5a5226259cbfa1990
kaeru <kaeru_geko2000@hotmail.co.jp>
2019-06-22 19:28:20 +0900

    マスターページ変更

--- public/js/masterpagecomponent.js
+++ public/js/masterpagecomponent.js
@@ -24,7 +24,7 @@ Vue.component('master-page', {
     template: '\
         <div class="columns">\
             <div class="column" style="margin-top:10px">\
-                <span class="icon" onclick="window.top.location.href = \'/Menu\';">\
+                <span class="icon" onclick="window.top.location.href = \'/index.html\';">\
                     <i class="fas fa-home"></i>\
                 </span>\
                 サンプルシステム（固定ページ）\
